8c208f9d2ab69fd63360ebc59cdd0d6dd8c2910a
richard <github@fantastic.me.uk>
2021-10-19 23:10:23 +0100

    multiple snakes

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,11 +1,22 @@
 import com.raylib.Jaylib.*
-import kotlin.jvm.JvmStatic
 import com.raylib.Raylib
-import com.raylib.Raylib.*
 import kotlinx.collections.immutable.*
-
+import java.util.*
 import kotlin.concurrent.thread
 
+
+typealias Vector2 = Pair<Int, Int>
+
+
+private val Vector2.x: Int
+    get() {
+        return first
+    }
+
+private val Vector2.y: Int
+    get() {
+        return second
+    }
 
 const val WIDTH = 1920
 const val HEIGHT = 1080
@@ -14,20 +25,30 @@ const val FRAME_TIME_NANOS: Long = 16000000
 
 const val RBL = 10
 
-class Vector2(val x: Int, val y: Int) {
-    override fun toString(): String {
-        return "($x, $y)"
-    }
-}
+
+//@JvmInline value class Vec(val v: Pair<Int, Int>){
+//    fun getX() = v.first
+//    fun getY() = v.second
+//}
+//
+
+
+//data class Vector2(val x: Int, val y: Int) {
+//    override fun toString(): String {
+//        return "($x, $y)"
+//    }
+//}
 
 enum class Direction(val x: Int, val y: Int) {
-    UP(0, 1), DOWN(0, -1), LEFT(-1, 0), RIGHT(1, 0), NONE (0,0), SPAWN(0,0)
+    UP(0, 1), DOWN(0, -1), LEFT(-1, 0), RIGHT(1, 0), CENTER(0, 0)
 }
 
+data class PlayerInput(val spawn: Boolean = false, val direction: Direction = Direction.CENTER)
+
 class Snake(x: Int = 0, y: Int = 0,
-        val segments: PersistentList<Segment> = persistentListOf(Segment(Vector2(x, y), Vector2(x, y))),
-        val direction: Direction = Direction.UP,
-        val color: Raylib.Color = RED
+            val segments: PersistentList<Segment> = persistentListOf(Segment(Vector2(x, y), Vector2(x, y))),
+            val direction: Direction = Direction.UP,
+            val color: Raylib.Color = RED
         //val position: Vector2 = Vector2(0, 0)
 ) {
 }
@@ -41,10 +62,10 @@ class Segment(val start: Vector2, val end: Vector2) {
 
 //class Point(val x: Int, val y: Int)
 
-class State(val snakes: PersistentMap<Int, Snake> = persistentMapOf()) {
+class WorldState(val snakes: PersistentMap<Int, Snake> = persistentMapOf()) {
 }
 
-class Input(val playerInputs: HashMap<Int, Direction> = HashMap())
+class InputState(val playerInputs: HashMap<Int, PlayerInput> = HashMap())
 
 object Main {
     val game = Game()
@@ -63,44 +84,52 @@ class Game {
     var updateTime: Long = 0
 
     @Volatile
-    var currentState = State(persistentMapOf())
-    var oldState = currentState
-    val inputArray = HashMap<Long, Input>()
+    var currentWorldState = WorldState(persistentMapOf())
+    var oldState = currentWorldState
+    val inputStateArray = HashMap<Long, InputState>()
 
     init {
         val snakes = HashMap<Int, Snake>()
         for (i in 1..1000) {
-            val color = ColorFromHSV(i.toFloat()*3, 1f, 1f)
-            val snake = Snake(x=i*10, y=i*10, color = color, direction = Direction.RIGHT)
+            val color = ColorFromHSV(i.toFloat() * 3, 1f, 1f)
+            val snake = Snake(x = i * 10, y = i * 10, color = color, direction = Direction.RIGHT)
             snakes.put(i, snake)
             //println("Snake $i, color ${snake.color.r()} ${snake.color.g()} ${snake.color.b()}")
         }
 
 
-        currentState = State(snakes.toImmutableMap().toPersistentMap())
-        oldState = currentState
+        currentWorldState = WorldState(snakes.toImmutableMap().toPersistentMap())
+        oldState = currentWorldState
     }
 
 
     fun run() {
+
+
+        val b = Vector2(2, 2)
+        b.x
+
+
+
         Raylib.InitWindow(WIDTH, HEIGHT, "Demo")
         Raylib.SetTargetFPS(60)
-
 
 
         val updateThread = thread {
             val t = System.nanoTime()
             update()
-            val d = System.nanoTime()-t
+            val d = System.nanoTime() - t
             println(d)
         }
 
         while (!Raylib.WindowShouldClose()) {
-            draw(currentState)
+            draw(currentWorldState)
         }
         Raylib.CloseWindow()
         updateThread.stop()
     }
+
+    var rand = Random()
 
     fun update() {
         var frame: Long = 0
@@ -113,33 +142,43 @@ class Game {
                 IsKeyDown(KEY_DOWN) -> Direction.DOWN
                 IsKeyDown(KEY_LEFT) -> Direction.LEFT
                 IsKeyDown(KEY_RIGHT) -> Direction.RIGHT
-                IsKeyDown(KEY_SPACE) -> Direction.SPAWN
-                else -> null
+                else -> Direction.CENTER
             }
-            if (inputDirection != null) {
-                val input = Input()
+            //   if (inputDirection != null) {
+            val inputState = InputState()
 
-                //var inputs = persistentMapOf<Int, Direction>()
-                //
-                input.playerInputs.put(0, inputDirection)
-                //}
-                inputArray.put(frame - 50, input)
-            }
+            //var inputs = persistentMapOf<Int, Direction>()
+            //
+
+            val spawn = IsKeyDown(KEY_SPACE)
+            val playerInput = PlayerInput(spawn, inputDirection)
+            inputState.playerInputs.put(0, playerInput)
+            //}
+            inputStateArray.put(frame - 50, inputState)
+            //   }
+
+
+//            for ((id, snake) in currentWorldState.snakes) {
+//                if (rand.nextFloat() < 0.01) {
+//                    inputState.playerInputs.put(id, Direction.values().random())
+//                    inputStateArray.put(frame, inputState)
+//                }
+//            }
 
             // if(frame>100) {
-            oldState = nextState(inputArray.getOrDefault(frame-101 , Input()), oldState)
+            oldState = nextState(inputStateArray.getOrDefault(frame - 101, InputState()), oldState)
             var tempState = oldState
-            for (i in frame - 100 .. frame) {
+            for (i in frame - 100..frame) {
                 //println(i)
-                tempState = nextState(inputArray.getOrDefault(i, Input()), tempState)
+                tempState = nextState(inputStateArray.getOrDefault(i, InputState()), tempState)
             }
 
-            currentState = tempState //nextState(inputArray.getOrDefault(frame, Input()), state)
+            currentWorldState = tempState //nextState(inputArray.getOrDefault(frame, Input()), state)
 
             // }
 
             val frameTimeTaken = System.nanoTime() - frameStartTime
-            updateTime = frameTimeTaken/1000000
+            updateTime = frameTimeTaken / 1000000
 
             frame++
             val timer = startTime + frame * FRAME_TIME_NANOS
@@ -153,30 +192,36 @@ class Game {
     }
 
 
-    fun nextState(inputs: Input, state: State): State {
-        var newSnakes = state.snakes
+    private fun nextState(inputState: InputState, worldState: WorldState): WorldState {
+        var newSnakes = worldState.snakes
 
 //        if(newSnakes.size==0){
 //            newSnakes = newSnakes.put(0, Snake())
 //        }
 
-        for ((id, input) in inputs.playerInputs){
-            if(input == Direction.SPAWN){
+        for ((id, playerInput) in inputState.playerInputs) {
+            if (playerInput.spawn) {
                 newSnakes = newSnakes.put(id, Snake())
             }
+
+            val snake = newSnakes.get(id) ?: Snake() // fixme
+            var segments = snake.segments
+
+            if (playerInput.direction != Direction.CENTER && snake.direction != playerInput.direction) {
+                val position = segments.last().end
+                segments = segments.add(Segment(position, position))
+            }
+
+
         }
 
         for ((id, snake) in newSnakes) {
             var segments = snake.segments
 
-            val input = inputs.playerInputs.get(id)
+            //val playerInput = inputState.playerInputs.get(id)
 
-            if (input != null && snake.direction != input) {
-                val position = segments.last().end
-                segments = segments.add(Segment(position, position))
-            }
 
-            val direction = input ?: snake.direction
+            val direction = playerInput?.direction ?: snake.direction
             val oldStart = segments.last().start
             val newPosition = Vector2(segments.last().end.x + direction.x, segments.last().end.y + direction.y)
             val newLastSegment = Segment(oldStart, newPosition)
@@ -186,7 +231,7 @@ class Game {
             val newSnake = Snake(0, 0, segments, direction, snake.color)
             newSnakes = newSnakes.put(id, newSnake)
         }
-        return State(newSnakes)
+        return WorldState(newSnakes)
 
     }
 
@@ -209,28 +254,32 @@ class Game {
     fun convertRL(p: Vector2, rlv: Raylib.Vector2): Raylib.Vector2 {
 
         return rlv.x((p.x / zoomX) + WIDTH / 2).y((p.y / zoomY) + HEIGHT / 2)
+
+
     }
 
 
-    private fun draw(state: State) {
+    private fun draw(worldState: WorldState) {
         BeginDrawing()
         ClearBackground(BLACK)
 
-        for (snake in state.snakes.values) {
+        for (snake in worldState.snakes.values) {
             //println("${snake.color.r()} ${snake.color.g()} ${snake.color.b()}")
             for (segment in snake.segments) {
-                val rlv = Raylib.Vector2()
-                val rlv2 = Raylib.Vector2()
-                rlv.deallocate(false)
-                rlv2.deallocate(false)
+                var rlv = Raylib.Vector2()
+                var rlv2 = Raylib.Vector2()
+                //rlv.deallocate(false)
+                //rlv2.deallocate(false)
 
                 DrawLineEx(convertRL(segment.start, rlv), convertRL(segment.end, rlv2), 22f, WHITE)
                 DrawLineEx(convertRL(segment.start, rlv), convertRL(segment.end, rlv2), 20f, snake.color)
                 DrawCircle(convertRL(segment.start, rlv).x().toInt(), convertRL(segment.start, rlv2).y().toInt(), 13f, WHITE)
-                DrawCircle(convertRL(segment.start, rlv).x().toInt(), convertRL(segment.start, rlv2).y().toInt(), 12f, snake.color)
 
+
+                DrawCircle(convertRL(segment.start, rlv).x().toInt(), convertRL(segment.start, rlv2).y().toInt(), 12f, snake.color)
                 DrawCircle(convertRL(segment.end, rlv).x().toInt(), convertRL(segment.end, rlv2).y().toInt(), 13f, WHITE)
                 DrawCircle(convertRL(segment.end, rlv).x().toInt(), convertRL(segment.end, rlv2).y().toInt(), 12f, snake.color)
+
 
                 rlv.deallocate()
                 rlv2.deallocate()
